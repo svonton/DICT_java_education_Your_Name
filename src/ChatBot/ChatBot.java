1846d93e09bd5946392ab9c53268279637fe14cf
svonton <svonton7@gmail.com>
2023-09-26 13:30:54 +0300

    ChatBot 1st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,9 @@
+package ChatBot;
+
+
+public class ChatBot {
+    public static void main(String[] args){
+        System.out.println("Hello! My name is MyBot.\n" +
+                "I was created in 2023.\n");
+    }
+}
