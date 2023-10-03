f3c696eecd355e5166331bd1807cba3f0c02d708
svonton <svonton7@gmail.com>
2023-10-03 16:31:22 +0300

    Hangman 1st stage

---
+++ src/Hangman/GameLogic.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class GameLogic {
+    public void greeting(){
+        System.out.println("HANGMAN\n" +
+                "The game will be available soon\n");
+    }
+}
---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,8 @@
+package Hangman;
+
+public class Hangman {
+    public static void main(String[] args){
+        GameLogic gameLogic = new GameLogic();
+        gameLogic.greeting();
+    }
+}
