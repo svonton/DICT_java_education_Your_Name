ef5c07e3e2889295b977d1da1572bff32b712e1e
svonton <svonton7@gmail.com>
2023-10-10 19:23:36 +0300

    CoffeeMachine 1st stage

---
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -0,0 +1,8 @@
+package CoffeeMachine;
+
+public class CoffeeMachine {
+    public static void main(String[] args){
+        MachineLogic machineLogic = new MachineLogic();
+        machineLogic.Greetings();
+    }
+}
