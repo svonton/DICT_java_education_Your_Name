0ce21a777b9595dffc97e7a3c8b0d516035b393a
svonton <svonton7@gmail.com>
2023-10-10 23:01:16 +0300

    CoffeeMachine 6th stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,8 +1,14 @@
 package CoffeeMachine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args){
         MachineLogic machineLogic = new MachineLogic();
-        machineLogic.machineAction();
+        Scanner scn = new Scanner(System.in);
+
+        while (!machineLogic.exitStatus){
+            machineLogic.updatedMachineAction(scn.nextLine());
+        }
     }
 }
--- src/CoffeeMachine/MachineLogic.java
+++ src/CoffeeMachine/MachineLogic.java
@@ -1,17 +1,14 @@
 package CoffeeMachine;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
-
 public class MachineLogic {
-    Scanner scn = new Scanner(System.in);
     private int water;
     private int milk;
     private int coffeeBeans;
     private int cups;
     private int money;
-    private boolean exitStatus;
+    public boolean exitStatus;
+    public String currentStatus;
+    private int fillStage;
     public MachineLogic(){
         this.milk = 540;
         this.coffeeBeans = 120;
@@ -19,6 +16,8 @@ public class MachineLogic {
         this.cups = 9;
         this.money = 550;
         this.exitStatus = false;
+        this.currentStatus = "Awake";
+        invMainMenu();
     }
     private boolean isPossible(int uWater, int uMilk, int uCoffeeBeans){
         if (uWater>water){System.out.println("Please restore water");return false;}
@@ -33,16 +32,14 @@ public class MachineLogic {
             cups -= 1;
             milk -= uMilk;
             money += uMoney;
-            remainingResources();
-        }else{
-            System.out.println("Canceled: not enough resources");
+            System.out.println("I have enough resources, making you a coffee!");
         }
+        invMainMenu();
+        this.currentStatus = "Awake";
     }
-    private void buyMenu(){
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back – to main menu:");
-        String uInp = userInput();
-        if(uInp.equals("back"))return;
-        int uCoffeeType = Integer.parseInt(uInp);
+    private void buyMenu(String outsideInput){
+        if(outsideInput.equals("back")){currentStatus = "Awake";invMainMenu();return;}
+        int uCoffeeType = Integer.parseInt(outsideInput);
         switch (uCoffeeType) {
             case 1 -> coffeeCook(250,0,16,4);
             case 2 -> coffeeCook(350,75,20,7);
@@ -50,21 +47,23 @@ public class MachineLogic {
             default -> System.out.println(String.format("Unknown coffee type: %d",uCoffeeType));
         }
     }
-    private void fillAction(){
-        System.out.println("\nWrite how many ml of water you want to add:");
-        water += Integer.parseInt(userInput());
-        System.out.println("Write how many ml of milk you want to add:");
-        milk += Integer.parseInt(userInput());
-        System.out.println("Write how many grams of coffee beans you want to add:");
-        coffeeBeans += Integer.parseInt(userInput());
-        System.out.println("Write how many cups you want to add:");
-        cups += Integer.parseInt(userInput());
-        remainingResources();
+    private void fillAction(String outsideInput){
+        switch (fillStage) {
+            case 1 -> {System.out.println("Write how many ml of milk you want to add:");
+                water += Integer.parseInt(outsideInput);fillStage = 2;}
+            case 2 -> {System.out.println("Write how many grams of coffee beans you want to add:");
+                milk += Integer.parseInt(outsideInput); fillStage = 3;}
+            case 3 -> {System.out.println("Write how many cups you want to add:");
+                coffeeBeans += Integer.parseInt(outsideInput); fillStage = 4;}
+            case 4 -> {
+                cups += Integer.parseInt(outsideInput); fillStage =0; currentStatus = "Awake";
+                invMainMenu();}
+        }
     }
     private void takeAction(){
         System.out.println(String.format("I gave you %d uah", money));
         money -= money;
-        remainingResources();
+        invMainMenu();
     }
     private void remainingResources(){
         System.out.println(String.format("""
@@ -74,27 +73,32 @@ public class MachineLogic {
                 %d of coffee beans
                 %d of disposable cups
                 %d of money""",water,milk,coffeeBeans,cups,money));
+        invMainMenu();
     }
-    private void machineMenu(String uChoice){
-        switch (uChoice) {
-            case "buy" -> buyMenu();
-            case "fill" -> fillAction();
-            case "take" -> takeAction();
-            case "remaining" -> remainingResources();
-            case "exit" -> exitStatus=true;
-            default -> System.out.println(String.format("Unknown command: %s",uChoice));
+    private void buyInvMenu(){
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back – to main menu:");
+        currentStatus = "buy";
+    }
+    private void fillInvMenu(){
+        currentStatus = "fill";
+        fillStage = 1;
+        System.out.println("Write how many ml of water you want to add:");
+    }
+    private void invMainMenu(){
+        System.out.println("Write action (buy, fill, take, remaining, exit):");
+    }
+    public void updatedMachineAction(String outsideInput){
+        if(currentStatus.equals("buy"))buyMenu(outsideInput);
+        else if (currentStatus.equals("fill")) fillAction(outsideInput);
+        else{
+            switch (outsideInput) {
+                case "buy" -> buyInvMenu();
+                case "fill" -> fillInvMenu();
+                case "take" -> takeAction();
+                case "remaining" -> remainingResources();
+                case "exit" -> exitStatus=true;
+                default -> System.out.println(String.format("Unknown command: %s",outsideInput));
+            }
         }
-    }
-
-    public void machineAction(){
-        while (!exitStatus){
-            System.out.println("Write action (buy, fill, take, remaining, exit):");
-            machineMenu(userInput());
-        }
-    }
-
-    private String userInput(){
-        System.out.print(">>> ");
-        return scn.nextLine();
     }
 }
