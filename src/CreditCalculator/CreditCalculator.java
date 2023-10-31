81ae00b5a4d347bce51e3d6f7aedf80c4c8c8515
svonton <svonton7@gmail.com>
2023-10-31 15:57:22 +0200

    CreditCalcularot 4th stage

--- src/CreditCalculator/CreditCalculator.java
+++ src/CreditCalculator/CreditCalculator.java
@@ -4,6 +4,11 @@ package CreditCalculator;
 public class CreditCalculator {
     public static void main(String[] args){
         CalculatorLogic calculatorLogic = new CalculatorLogic();
-        calculatorLogic.calculation();
+        String type = System.getProperty("type");
+        String principal = System.getProperty("principal"); // P
+        String periods = System.getProperty("periods"); // n
+        String interest = System.getProperty("interest"); // for i calc
+        String payment = System.getProperty("payment"); // A
+        calculatorLogic.dataAnalyze(type,principal,periods,interest,payment);
     }
 }
