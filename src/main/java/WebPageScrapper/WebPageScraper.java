d8c8746e6be1858aee5991c39ae000dff04c4e23
svonton <svonton7@gmail.com>
2023-11-28 19:56:20 +0200

    WebPageScraper 3.5-4.5 stage

--- src/main/java/WebPageScrapper/WebPageScraper.java
+++ src/main/java/WebPageScrapper/WebPageScraper.java
@@ -6,6 +6,6 @@ public class WebPageScraper {
     public static void main(String[] arg) {
         Scanner scanner = new Scanner(System.in);
         WebPageScraperLogic webPageScraperLogic = new WebPageScraperLogic();
-        webPageScraperLogic.mainProcessing(scanner.nextLine());
+        webPageScraperLogic.mainProcessing("https://www.nature.com/nature/articles?sort=PubDate&year=2022&page=3");
     }
 }
--- src/main/java/WebPageScrapper/WebPageScraperLogic.java
+++ src/main/java/WebPageScrapper/WebPageScraperLogic.java
@@ -2,6 +2,8 @@ package WebPageScrapper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
@@ -11,11 +13,20 @@ public class WebPageScraperLogic {
     public void mainProcessing(String inp) {
         try {
             Document doc = Jsoup.connect(inp).get();
-            String pageContent = doc.html();
-            BufferedWriter writer = new BufferedWriter(new FileWriter("source.html"));
-            writer.write(pageContent);
-            writer.close();
-            System.out.println("Content saved.");
+            Elements articles = doc.select("article");
+            for(Element article : articles){
+                if(article.select("span[class=c-meta__type]").text().matches("News")){
+                    String fileName = article.select("a").text().replaceAll(" ","_");
+                    System.out.println(fileName);
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("articles/"+fileName+".txt"));
+                    Element link = article.select("h3.c-card__title > a").first();
+                    String url = link.attr("href");
+                    Document fullArticleDoc = Jsoup.connect("https://www.nature.com"+url).get();
+                    System.out.println(fullArticleDoc.select("p[class=article__teaser]").text());
+                    writer.write(fullArticleDoc.select("p[class=article__teaser]").text());
+                    writer.close();
+                }
+            }
         } catch (org.jsoup.HttpStatusException e) {
             System.out.println(String.format("The URL returned %s!",e.getStatusCode()));
         } catch (IOException e) {
