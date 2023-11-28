5ce8fe810ec098f4882b97c0801aa753e738c036
svonton <svonton7@gmail.com>
2023-11-28 18:10:00 +0200

    WebPageScraper 3rd stage

--- src/main/java/WebPageScrapper/WebPageScraper.java
+++ src/main/java/WebPageScrapper/WebPageScraper.java
@@ -1,6 +1,5 @@
 package WebPageScrapper;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class WebPageScraper {
--- src/main/java/WebPageScrapper/WebPageScraperLogic.java
+++ src/main/java/WebPageScrapper/WebPageScraperLogic.java
@@ -1,30 +1,25 @@
 package WebPageScrapper;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class WebPageScraperLogic {
     public void mainProcessing(String inp) {
         try {
-            if (!inp.contains("https://www.imdb.com/title/")) {
-                System.out.println("Invalid movie page!");
-                return;
-            }
-            Connection connection = Jsoup.connect(inp);
-            connection.header("Accept-Language", "en-US,en;q=0.5");
-            Document doc = connection.get();
-            Element metaDescription = doc.select("meta[name=description]").first();
-            if (metaDescription != null) {
-                String content = metaDescription.attr("content");
-                System.out.println(content);
-            } else {
-                System.out.println("Description not found.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+            Document doc = Jsoup.connect(inp).get();
+            String pageContent = doc.html();
+            BufferedWriter writer = new BufferedWriter(new FileWriter("source.html"));
+            writer.write(pageContent);
+            writer.close();
+            System.out.println("Content saved.");
+        } catch (org.jsoup.HttpStatusException e) {
+            System.out.println(String.format("The URL returned %s!",e.getStatusCode()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
